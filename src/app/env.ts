3554ba780a730d0e2839c6888985e07cee3dd0ad
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-05-31 18:38:04 +0800

    Setup firebase credentials

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,13 @@
 export const env = {
   DEFAULT: {
     userImagePlaceholder: 'assets/imgs/user_placeholder.png'
+  },
+  FIREBASE: {
+    apiKey: 'AIzaSyA-AkfUkB2ESqsvmlUqBfYdzFz-D_J932E',
+    authDomain: 'black-vegan-meet.firebaseapp.com',
+    databaseURL: 'https://black-vegan-meet.firebaseio.com',
+    projectId: 'black-vegan-meet',
+    storageBucket: 'black-vegan-meet.appspot.com',
+    messagingSenderId: '237081629673'
   }
-}
+};
