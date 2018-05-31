35f0ed97409d3fb6cee27ac6cbbc8bd50f511308
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-06-01 00:43:06 +0800

    Pointed firebase endpoints to dev for easy access to the developers

--- src/app/env.ts
+++ src/app/env.ts
@@ -2,12 +2,22 @@ export const env = {
   DEFAULT: {
     userImagePlaceholder: 'assets/imgs/user_placeholder.png'
   },
+  // PROD
+  // FIREBASE: {
+  //   apiKey: 'AIzaSyA-AkfUkB2ESqsvmlUqBfYdzFz-D_J932E',
+  //   authDomain: 'black-vegan-meet.firebaseapp.com',
+  //   databaseURL: 'https://black-vegan-meet.firebaseio.com',
+  //   projectId: 'black-vegan-meet',
+  //   storageBucket: 'black-vegan-meet.appspot.com',
+  //   messagingSenderId: '237081629673'
+  // }
+  // DEV
   FIREBASE: {
-    apiKey: 'AIzaSyA-AkfUkB2ESqsvmlUqBfYdzFz-D_J932E',
-    authDomain: 'black-vegan-meet.firebaseapp.com',
-    databaseURL: 'https://black-vegan-meet.firebaseio.com',
-    projectId: 'black-vegan-meet',
-    storageBucket: 'black-vegan-meet.appspot.com',
-    messagingSenderId: '237081629673'
+    apiKey: 'AIzaSyBf9FgZ2RpR6bHNDEPB4OSZDAaKP5BD9aw',
+    authDomain: 'bvm-dev-firebase.firebaseapp.com',
+    databaseURL: 'https://bvm-dev-firebase.firebaseio.com',
+    projectId: 'bvm-dev-firebase',
+    storageBucket: 'bvm-dev-firebase.appspot.com',
+    messagingSenderId: '1038454495212'
   }
 };
