4766ec58600a7eb75457373434b2b31eb18a307c
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-06-02 20:17:16 +0800

    Adjusted page headers and added Advice Corner Page

---
+++ src/pages/advice-corner/advice-corner.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AdviceCornerPage } from './advice-corner';
+
+@NgModule({
+  declarations: [
+    AdviceCornerPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AdviceCornerPage),
+  ],
+})
+export class AdviceCornerPageModule {}
