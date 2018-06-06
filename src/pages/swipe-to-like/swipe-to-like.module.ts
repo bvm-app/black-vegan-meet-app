e2185da90090eeb03a48f4f1a0cc7c1bb04bb73f
grphx <grphxprod@gmail.com>
2018-06-07 02:57:39 +0800

    added ability to swipe to like images

--- src/pages/swipe-to-like/swipe-to-like.module.ts
+++ src/pages/swipe-to-like/swipe-to-like.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SwipeToLikePage } from './swipe-to-like';
 
+import { SwipeCardsModule } from 'ng2-swipe-cards';
+
 @NgModule({
   declarations: [
     SwipeToLikePage,
   ],
   imports: [
     IonicPageModule.forChild(SwipeToLikePage),
+    SwipeCardsModule
   ],
 })
 export class SwipeToLikePageModule {}
