7675534a1c294d0e9442f5d7fd50dd1d9c5fc78b
grphx <grphxprod@gmail.com>
2018-06-07 02:57:39 +0800

    added ability to swipe to like images

--- src/pages/swipe-to-like/swipe-to-like.ts
+++ src/pages/swipe-to-like/swipe-to-like.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { env } from '../../app/env';
+import { Subscription } from 'rxjs/Subscription';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { IUser } from '../../models/IUser';
 
 /**
  * Generated class for the SwipeToLikePage page.
@@ -15,11 +20,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SwipeToLikePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cardDirection = "xy";
+  cardOverlay: any = {
+    like: {
+      backgroundColor: '#28e93b'
+    },
+    dislike: {
+      backgroundColor: '#e92828'
+    }
+  };
+
+  defaultUserImage = env.DEFAULT.userImagePlaceholder;
+
+  users: IUser[];
+  potentialMatches: any = [];
+  usersSubscription: Subscription;
+
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private sanitizer: DomSanitizer,
+    private db: AngularFireDatabase
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SwipeToLikePage');
+    this.generatePotentialMatches();
   }
+
+  ionViewDidLeave() {
+    if (this.usersSubscription) this.usersSubscription.unsubscribe();
+  }
+
+  onCardInteract(event, user) {
+    console.log('on card interact user', user);
+  }
+
+  generatePotentialMatches() {
+
+    this.usersSubscription = this.db.list(`/userData`).valueChanges().subscribe((users: IUser[]) => {
+      //TODO change implementation typecast users
+      this.users = users;
+      this.potentialMatches = this.users;
+    });
+
+    this.potentialMatches.forEach(element => {
+      this.potentialMatches.push({
+        likeEvent: new EventEmitter(),
+        destroyEvent: new EventEmitter()
+      });
+    });
+  }
+
 
 }
