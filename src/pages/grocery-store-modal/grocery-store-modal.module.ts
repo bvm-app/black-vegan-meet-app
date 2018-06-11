42054e93911c8c479c57a47642b363e1c7690c50
Jamie joe van stone <jamiejoevanstone@gmail.com>
2018-06-11 13:44:03 +0800

    fixed modal errors.
    Added image upload container

--- src/pages/grocery-store-modal/grocery-store-modal.module.ts
+++ src/pages/grocery-store-modal/grocery-store-modal.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { DragulaModule } from 'ng2-dragula';
 import { GroceryStoreModalPage } from './grocery-store-modal';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 @NgModule({
   declarations: [
     GroceryStoreModalPage,
   ],
   imports: [
+    LazyLoadImageModule,
+    DragulaModule,
     IonicPageModule.forChild(GroceryStoreModalPage),
   ],
 })
